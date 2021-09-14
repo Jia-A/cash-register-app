51a12721ee1065baa3801cff228f3543a129b5e9
Jia-A <agrawaljiya2011@gmail.com>
2021-09-14 15:14:04 +0530

    second-commit

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 var billInput = document.querySelector("#billamount");
 
 var cashGiven = document.querySelector("#cashgiven");
-console.log(billInput.value)
-console.log(cashGiven.value);
+var buttonCheck = document.querySelector("#check-btn");
+var message = document.querySelector("#error-message");
+
+buttonCheck.addEventListener("click", function valueValidation(){
+    message.style.display = "none";
+
+    if(Number(billInput.value) > 0){
+        if(Number(cashGiven.value)>Number(billInput.value)){
+
+        }
+        else{
+            showMessage("Cash given should be more than the bill");
+        };
+    }
+    else{
+        showMessage("Invalid input");
+    }
+});
+function showMessage(message){
+    message.style.display = "block";
+    message.innerText = message;
+}
